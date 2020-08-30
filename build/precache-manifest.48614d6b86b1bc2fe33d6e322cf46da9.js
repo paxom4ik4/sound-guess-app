0ce2edd2886d52c7606394a1d73af697e5652e00
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-08-30 13:56:04 +0300

    feat: prod. build created

---
+++ build/precache-manifest.48614d6b86b1bc2fe33d6e322cf46da9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dabd442b870a5f7b13c59a8ec9c9b72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45083e90e6e89ca1baf2",
+    "url": "/static/css/2.9131a3d6.chunk.css"
+  },
+  {
+    "revision": "b39f2c6ee59e30e23ac6",
+    "url": "/static/css/main.8b91479c.chunk.css"
+  },
+  {
+    "revision": "45083e90e6e89ca1baf2",
+    "url": "/static/js/2.a5927e1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5927e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39f2c6ee59e30e23ac6",
+    "url": "/static/js/main.82d15acc.chunk.js"
+  },
+  {
+    "revision": "e0c0f7090ba4df3598aa",
+    "url": "/static/js/runtime-main.661578d6.js"
+  }
+]);
